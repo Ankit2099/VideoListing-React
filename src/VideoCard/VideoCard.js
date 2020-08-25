6c86303eee1f4cd730bf14b597f1365074ea3912
Qaifi Khan <qaifikhan@gmail.com>
2020-08-25 18:11:28 +0530

    first commit

---
+++ src/VideoCard/VideoCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './VideoCard.module.css';
+
+const VideoCard = (props) => {
+    return(
+        <div className={classes.VideoCard}>
+            <img className={classes.Thumbnail} src={props.thumbnail} alt="Video Thumbnail" />
+            <h3 className={classes.Title}>{props.title}</h3>
+        </div>
+    );
+}
+
+export default VideoCard;
