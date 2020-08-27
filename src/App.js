ea997c1aed5e91f4e5190e9ddbf48211eab64036
Qaifi Khan <qaifikhan@gmail.com>
2020-08-27 21:00:17 +0530

    Starter code for component updates;

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import React from 'react';
+import axios from 'axios';
+
+import Button from './Button/Button';
 
 import classes from './App.module.css';
 
-function App() {
-  return (
-    <div className={classes.App}>
-      <h1 className={classes.MainHeading}>Video Grid!!</h1>
-      <div className={classes.VideoGrid}>
-        {/* Add Video Cards from Video Array available in Constants  */}
+class App extends React.Component {
+  state = {
+    totalCount: 0,
+    totalLikeCount: 1,
+    totalDislikeCount: 1,
+  }
+
+  handleIncrementClick = () => {
+    this.setState((oldState) => {
+      return {
+        totalCount: oldState.totalCount + 1, 
+      }
+    });
+  }
+  
+  handleDecrementClick = () => {
+    this.setState({
+      totalCount: this.state.totalCount - 1, 
+    });
+  }
+
+  componentDidUpdate() {
+    console.log('App Updated!!')
+  }
+
+  render() {
+    console.log('Render App')
+    return (
+      <div className={classes.App}>
+        <h1>Current Count: {this.state.totalCount}</h1>
+        <p>Min count:0 and Max count: 5</p>
+        <Button onBtnClick={this.handleIncrementClick} label={"Count++"} totalCount={this.state.totalCount} />
+        <Button onBtnClick={this.handleDecrementClick} label={"Count--"} totalCount={this.state.totalCount} />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/VideoCard/VideoCard.js
+++ src/VideoCard/VideoCard.js
@@ -2,8 +2,9 @@ import React from 'react';
 import classes from './VideoCard.module.css';
 
 const VideoCard = (props) => {
+
     return(
-        <div className={classes.VideoCard}>
+        <div className={classes.VideoCard} onClick={() => alert(`Clicked for ${props.id}`)}>
             <img className={classes.Thumbnail} src={props.thumbnail} alt="Video Thumbnail" />
             <h3 className={classes.Title}>{props.title}</h3>
         </div>
